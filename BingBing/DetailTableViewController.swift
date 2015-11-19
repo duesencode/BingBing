2c02857bce5ac1909a2d3f64cea98a9fa56fa8b5
Dirk Fritz <df038@hdm-stuttgart.de>
2015-11-19 17:08:14 +0100

    First commit

---
+++ BingBing/DetailTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailTableViewController.swift
+//  BingBing
+//
+//  Created by Dirk Fritz on 19.11.15.
+//  Copyright © 2015 Dirk Fritz. All rights reserved.
+//
+
+import UIKit
+
+class DetailTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.navigationController?.toolbarHidden = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
